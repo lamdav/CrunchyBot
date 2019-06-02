ad0bf6a6ae4548315de6fa1aee3e8c33070a6c33
David Lam <dav_lam@ymail.com>
2019-06-02 14:19:40 -0700

    fix execute entrypoint.sh

--- actions/execute/entrypoint.sh
+++ actions/execute/entrypoint.sh
@@ -5,7 +5,7 @@ set -eu
 echo "#########################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
-crunchy_bot publish
+crunchy publish
 
 echo "#########################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
