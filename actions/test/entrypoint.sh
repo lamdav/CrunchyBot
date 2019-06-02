226791a1f8b7fbd1ae363e51d7344a4487b28431
David Lam <dav_lam@ymail.com>
2019-06-02 02:38:29 -0700

    add git

--- actions/test/entrypoint.sh
+++ actions/test/entrypoint.sh
@@ -5,7 +5,6 @@ set -eu
 echo "#########################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
-ls
 python setup.py pytest
 
 echo "#########################################"
