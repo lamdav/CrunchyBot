712af06f544aab62d9fa02c1a4ec13f1638c432e
David Lam <dav_lam@ymail.com>
2019-06-02 23:28:37 -0700

    fix bug with environment fallback

--- crunchy_bot/config/json_config_parser.py
+++ crunchy_bot/config/json_config_parser.py
@@ -15,50 +15,65 @@ class JsonConfigParser(ConfigParser):
     def parse(self, path: pathlib.Path) -> Config:
         self.logger.info("Fetching Account Data...")
         posix_path = path.resolve().as_posix()
-        with open(posix_path, "r") as data_file:
-            data_dictionary = json.load(data_file)
+        data_dictionary = {}
+        try:
+            with open(posix_path, "r") as data_file:
+                data_dictionary = json.load(data_file)
+        except FileNotFoundError as e:
+            self.logger.warn(f"Unable to read {posix_path}: {str(e)}")
+            self.logger.info("Attempting to load from environment")
 
         crunchy_username = data_dictionary.get(
             "crunchy_username", os.environ.get("CRUNCHY_USERNAME")
         )
         if crunchy_username is None:
-            raise ValueError(f"Missing crunchy_username in {posix_path}")
+            raise ValueError(
+                f"Missing crunchy_username in {posix_path} and environment"
+            )
 
         crunchy_password = data_dictionary.get(
             "crunchy_password", os.environ.get("CRUNCHY_PASSWORD")
         )
         if crunchy_password is None:
-            raise ValueError(f"Missing crunchy_password in {posix_path}")
+            raise ValueError(
+                f"Missing crunchy_password in {posix_path} and environment"
+            )
 
         reddit_client_id = data_dictionary.get(
             "reddit_client_id", os.environ.get("REDDIT_CLIENT_ID")
         )
         if reddit_client_id is None:
-            raise ValueError(f"Missing reddit_client_id in {posix_path}")
+            raise ValueError(
+                f"Missing reddit_client_id in {posix_path} and environment"
+            )
 
         reddit_client_secret = data_dictionary.get(
             "reddit_client_secret", os.environ.get("REDDIT_CLIENT_SECRET")
         )
         if reddit_client_secret is None:
-            raise ValueError(f"Missing reddit_client_secret in {posix_path}")
+            raise ValueError(
+                f"Missing reddit_client_secret in {posix_path} and environment"
+            )
 
         reddit_user_agent = data_dictionary.get(
             "reddit_user_agent", os.environ.get("REDDIT_USER_AGENT")
         )
         if reddit_user_agent is None:
-            raise ValueError(f"Missing reddit_user_agent in {posix_path}")
+            raise ValueError(
+                f"Missing reddit_user_agent in {posix_path} and environment"
+            )
 
         reddit_username = data_dictionary.get(
             "reddit_username", os.environ.get("REDDIT_USERNAME")
         )
         if reddit_username is None:
-            raise ValueError(f"Missing reddit_username in {posix_path}")
+            raise ValueError(f"Missing reddit_username in {posix_path} and environment")
 
         reddit_password = data_dictionary.get(
             "reddit_password", os.environ.get("REDDIT_PASSWORD")
         )
         if reddit_password is None:
-            raise ValueError(f"Missing reddit_password in {posix_path}")
+            raise ValueError(f"Missing reddit_password in {posix_path} and environment")
 
         log_dir = data_dictionary.get("log_dir", os.environ.get("LOG_DIR"))
         if log_dir is None:
--- crunchy_bot/config/test_json_config_parser.py
+++ crunchy_bot/config/test_json_config_parser.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from unittest import mock
 from unittest.mock import mock_open, patch
@@ -19,22 +20,45 @@ def json_config_parser(logger: Logger) -> ConfigParser:
     return JsonConfigParser(logger=logger)
 
 
-def test_parse(logger: Logger, json_config_parser: ConfigParser):
+@pytest.fixture()
+def config_data() -> dict:
+    return {
+        "crunchy_username": "crunchy_user",
+        "crunchy_password": "crunchy_pass",
+        "reddit_client_id": "client_id",
+        "reddit_client_secret": "client_secret",
+        "reddit_user_agent": "CrunchyBot:v4.0.0 (hosted by /u/{YOUR_USERNAME})",
+        "reddit_username": "reddit_user",
+        "reddit_password": "reddit_pass",
+        "log_dir": "/tmp/crunchybot/logs",
+    }
+
+
+def test_parse(logger: Logger, json_config_parser: ConfigParser, config_data: dict):
     with patch("crunchy_bot.config.json_config_parser.open", mock_open()) as open_mock:
-        with patch(
-            "crunchy_bot.config.json_config_parser.json", autospec=True
-        ) as json_mock:
-            data = {
-                "crunchy_username": "crunchy_user",
-                "crunchy_password": "crunchy_pass",
-                "reddit_client_id": "client_id",
-                "reddit_client_secret": "client_secret",
-                "reddit_user_agent": "CrunchyBot:v4.0.0 (hosted by /u/{YOUR_USERNAME})",
-                "reddit_username": "reddit_user",
-                "reddit_password": "reddit_pass",
-                "log_dir": "/tmp/crunchybot/logs",
-            }
-            json_mock.load.return_value = data
+        with patch("crunchy_bot.config.json_config_parser.json") as json_mock:
+            json_mock.load.return_value = config_data
+
+            config_path = Path.home()
+            config = json_config_parser.parse(config_path)
+
+            logger.info.assert_any_call("Fetching Account Data...")
+            open_mock.assert_any_call(config_path.resolve().as_posix(), "r")
+            json_mock.load.assert_called()
+            logger.info.assert_any_call("Fetched Account Data")
+
+            assert config == Config(**config_data)
+
+
+def test_parse_file_not_found_exception(
+    logger: Logger, json_config_parser: ConfigParser, config_data: dict
+):
+    with patch("crunchy_bot.config.json_config_parser.open", mock_open()) as open_mock:
+        with patch("crunchy_bot.config.json_config_parser.json") as json_mock:
+            open_mock.side_effect = FileNotFoundError
+
+            for k, v in config_data.items():
+                os.environ[k.upper()] = v
 
             config_path = Path.home()
             config = json_config_parser.parse(config_path)
@@ -42,5 +66,6 @@ def test_parse(logger: Logger, json_config_parser: ConfigParser):
             logger.info.assert_any_call("Fetching Account Data...")
             open_mock.assert_any_call(config_path.resolve().as_posix(), "r")
             logger.info.assert_any_call("Fetched Account Data")
+            json_mock.load.assert_not_called()
 
-            assert config == Config(**data)
+            assert config == Config(**config_data)
